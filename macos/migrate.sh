74012cd59eaa627721588be722171a7a3ecbd442
Garthylou Jean-Baptiste Gury <garthylou@gmx.com>
2016-10-18 12:53:56 +0200

    Fix right on cache directory when installing package.

--- macos/migrate.sh
+++ macos/migrate.sh
@@ -5,8 +5,9 @@ myuser=${SUDO_USER:-$USER}
 chown -R ${myuser} "/Users/${myuser}/Library/Application Support/Libreosteo"
 agents_dir="/Users/${myuser}/Library/LaunchAgents"
 mkdir -p ${agents_dir}
-cp /Applications/Libreosteo.app/Contents/Resources/macos/org.libreosteo.macos.Libreosteo.plist ${agents_dir}/.
+cp org.libreosteo.macos.Libreosteo.plist ${agents_dir}/.
 chmod +x "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
+chmod -R 777 /Applications/Libreosteo.app/Contents/Resources/static/CACHE
 chown -R ${myuser} "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
 sudo -u ${myuser} launchctl load -w "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
 sudo -u ${myuser} launchctl start org.libreosteo.macos.Libreosteo
