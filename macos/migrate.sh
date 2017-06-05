319a7ee2223928abf8fbc20689091b6149c87697
Garthylou Jean-Baptiste Gury <garthylou@gmx.com>
2017-06-05 21:32:44 +0200

    Integrate property list into migrate script

--- macos/migrate.sh
+++ macos/migrate.sh
@@ -5,7 +5,26 @@ myuser=${SUDO_USER:-$USER}
 chown -R ${myuser} "/Users/${myuser}/Library/Application Support/Libreosteo"
 agents_dir="/Users/${myuser}/Library/LaunchAgents"
 mkdir -p ${agents_dir}
-cp org.libreosteo.macos.Libreosteo.plist ${agents_dir}/.
+(
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
+echo "<plist version=\"1.0\">"
+echo "<dict>"
+echo "	<key>KeepAlive</key>"
+echo "	<true/>"
+echo "	<key>Label</key>"
+echo "	<string>org.libreosteo.macos.Libreosteo</string>"
+echo "	<key>ProgramArguments</key>"
+echo "	<array>"
+echo "		<string>/Applications/Libreosteo.app/Contents/MacOS/server</string>"
+echo "	</array>"
+echo "	<key>RunAtLoad</key>"
+echo "	<true/>"
+echo "	<key>ProcessType</key>"
+echo "	<string>Background</string>"
+echo "</dict>"
+echo "</plist>"
+) > ${agents_dir}/org.libreosteo.macos.Libreosteo.plist
 chmod +x "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
 chmod -R 777 /Applications/Libreosteo.app/Contents/Resources/static/CACHE
 chown -R ${myuser} "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
